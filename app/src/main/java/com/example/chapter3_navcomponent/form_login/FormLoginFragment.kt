4580cd903b28e7d693d339959dbb8fd74f40c4c1
surezluvy <raynaldizoel@gmail.com>
2022-09-05 21:27:27 +0700

    Latihan Navigate Login Form

---
+++ app/src/main/java/com/example/chapter3_navcomponent/form_login/FormLoginFragment.kt
@@ -0,0 +1,42 @@
+package com.example.chapter3_navcomponent.form_login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.chapter3_navcomponent.R
+import kotlinx.android.synthetic.main.fragment_form_login.*
+
+class FormLoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_form_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnLogin.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.homeFragment)
+        }
+        lupaPassword.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.lupasFragment)
+        }
+        register.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.registerFragment)
+        }
+        btnChat.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.chatFragment)
+        }
+        btnHelp.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.helpFragment)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/chapter3_navcomponent/form_login/LoginMainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.chapter3_navcomponent.form_login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.chapter3_navcomponent.R
+
+class LoginMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_main)
+    }
+}
