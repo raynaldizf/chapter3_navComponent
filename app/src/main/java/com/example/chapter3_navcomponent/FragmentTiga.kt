7f0953fee861077b68a7bcd05dab0ac82dbdf24b
surezluvy <raynaldizoel@gmail.com>
2022-09-05 19:58:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/chapter3_navcomponent/FragmentTiga.kt
@@ -0,0 +1,30 @@
+package com.example.chapter3_navcomponent
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_satu.*
+import kotlinx.android.synthetic.main.fragment_tiga.*
+
+class FragmentTiga : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tiga, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnToSatu.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.fragmentSatu)
+        }
+    }
+
+}
